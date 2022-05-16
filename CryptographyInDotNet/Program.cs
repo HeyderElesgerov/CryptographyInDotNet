9360cec5008c47017ef1c4d3f98b704a1070edc2
heyder <elesgerovheyder@gmail.com>
2022-05-16 22:54:29 +0400

    Add file comparer

--- CryptographyInDotNet/Program.cs
+++ CryptographyInDotNet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace CryptographyInDotNet
 {
@@ -6,6 +7,7 @@ namespace CryptographyInDotNet
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
---
+++ FileComparer/FileComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FileComparer
+{
+    public class FileComparer
+    {
+        public bool IsEqual(string path1, string path2)
+        {
+            byte[] file1Bytes = getBytes(path1);
+            byte[] file2Bytes = getBytes(path2);
+            SHA256 sha256 = SHA256.Create();
+            byte[] file1Hash = sha256.ComputeHash(file1Bytes);
+            byte[] file2Hash = sha256.ComputeHash(file2Bytes);
+            if (file1Hash.Length != file2Hash.Length)
+                return false;
+            for (int i = 0; i < file1Hash.Length; i++)
+            {
+                if (file1Hash[i] != file2Hash[i])
+                    return false;
+            }
+            return true;
+        }
+
+        private byte[] getBytes(string path)
+        {
+            using StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open));
+            string content = sr.ReadToEnd();
+            return Encoding.UTF8.GetBytes(content);
+        }
+    }
+}
