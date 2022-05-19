a552a69b649ecdba44688c808477d0e98c5c8ec9
heyder <elesgerovheyder@gmail.com>
2022-05-19 11:59:42 +0400

    Add compressed encryption

--- CryptographyInDotNet/Program.cs
+++ CryptographyInDotNet/Program.cs
@@ -5,14 +5,24 @@ namespace CryptographyInDotNet
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void ShowEncryptionAndDecryption(string text)
+        { 
             byte[] key = { 23, 123, 11, 45, 90, 23, 123, 93, 10, 53, 43, 26, 40, 18, 145, 140};
             byte[] iv = { 25, 125, 13, 47, 92, 25, 125, 95, 15, 55, 45, 28, 42, 20, 147, 142};
-            string encryptedText = SymmetricEncryption.Encrypt("Salam", key, iv);
+            string originalData = text;
+            string encryptedText = SymmetricEncryption.Encrypt(originalData, key, iv);
+            string compressedEncryptedText = SymmetricEncryptionWithCompression.Encrypt(originalData, key, iv);
             Console.WriteLine("Encrypted text: " + encryptedText);
-            string decryptedText = SymmetricEncryption.Decrypt(key, iv, encryptedText);
+            Console.WriteLine("Compressed Encrypted text: " + compressedEncryptedText);
+            
+            string decryptedText = SymmetricEncryption.Decrypt(key, iv, encryptedText);;
+            string compressedDecryptedText = SymmetricEncryptionWithCompression.Decrypt(key, iv, compressedEncryptedText);
             Console.WriteLine("Original data: " + decryptedText);
+            Console.WriteLine("Compressed Original data: " + compressedDecryptedText);
+        }
+        static void Main(string[] args)
+        {
+            ShowEncryptionAndDecryption("");
         }
     }
 }
---
+++ Encryption/SymmetricEncryptionWithCompression.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption
+{
+    public class SymmetricEncryptionWithCompression
+    {
+        public static string Encrypt(string text, byte[] key, byte[] iv)
+        {
+            byte[] original = Encoding.UTF8.GetBytes(text);
+            using var symmetricAlg = Aes.Create();
+            using var encryptor = symmetricAlg.CreateEncryptor(key, iv);
+            string path = GetRandomFilePath();
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                using (var cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Write))
+                {
+                    using (var ds = new DeflateStream(cs, CompressionMode.Compress))
+                    {
+                        ds.Write(original);   
+                    }
+                }
+            }
+
+            byte[] buffer = File.ReadAllBytes(path);
+            File.Delete(path);
+            return Convert.ToBase64String(buffer);
+        }
+
+        public static string Decrypt(byte[] key, byte[] iv, string text)
+        {
+            using var symmetricAlg = Aes.Create();
+            using var decrypt = symmetricAlg.CreateDecryptor(key, iv);
+            byte[] encryptedBytes = Convert.FromBase64String(text);
+            string path = GetRandomFilePath();
+            File.WriteAllBytes(path, encryptedBytes);
+            using (var fs = new FileStream(path, FileMode.Open))
+            {
+                using (var cs = new CryptoStream(fs, decrypt, CryptoStreamMode.Read))
+                {
+                    using (var ds = new DeflateStream(cs, CompressionMode.Decompress))
+                    {
+                        using (var sr = new StreamReader(ds))
+                        {
+                            string originalData = sr.ReadToEnd();
+                            File.Delete(path);
+                            return originalData;
+                        }
+                    }
+                }
+            }
+        }
+        private static string GetRandomFilePath() => Path.Combine(Environment.CurrentDirectory, Guid.NewGuid() + ".txt");
+    }
+}
